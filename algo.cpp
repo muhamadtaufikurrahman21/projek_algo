0adf420eda0c613b1f9b528d7921a3ca675713a4
muhamadtaufikurrahman21 <muhamadtaufikurrahman21@gmail.com>
2026-05-20 19:36:33 +0700

    upload projek_algo

---
+++ algo.cpp
@@ -0,0 +1,285 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+// 1. STRUKTUR DATA
+struct Obat {
+    int id;
+    char nama[30];
+    int stok;
+    long harga;
+};
+
+typedef struct Node *typeptr;
+struct Node {
+    Obat info;
+    typeptr kiri;  // Pointer sebelumnya
+    typeptr kanan; // Pointer berikutnya
+};
+
+typeptr awal = NULL, akhir = NULL;
+
+// 2. MANAJEMEN LINKED LIST
+void buatListBaru() {
+    awal = NULL;
+    akhir = NULL;
+}
+
+int listKosong() {
+    return (awal == NULL);
+}
+
+void tambahObat(Obat dataBaru) {
+    typeptr NB = new Node;
+    NB->info = dataBaru;
+    NB->kiri = NULL;
+    NB->kanan = NULL;
+
+    if (listKosong()) {
+        awal = NB;
+        akhir = NB;
+    } else {
+        NB->kiri = akhir;
+        akhir->kanan = NB;
+        akhir = NB;
+    }
+}
+
+// 3. PENGURUTAN QUICK SORT
+// Mode: 1=ID (Asc), 2=Harga (Asc), 3=Harga (Desc)
+void quickSort(Obat arr[], int low, int high, int mode) {
+    int i = low, j = high;
+    Obat temp, pivot = arr[(low + high) / 2];
+
+    while (i <= j) {
+        if (mode == 1) { // Berdasarkan ID (Asc)
+            while (arr[i].id < pivot.id) i++;
+            while (arr[j].id > pivot.id) j--;
+        } else if (mode == 2) { // Harga (Asc)
+            while (arr[i].harga < pivot.harga) i++;
+            while (arr[j].harga > pivot.harga) j--;
+        } else if (mode == 3) { // Harga (Desc)
+            while (arr[i].harga > pivot.harga) i++;
+            while (arr[j].harga < pivot.harga) j--;
+        }
+
+        if (i <= j) {
+            temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+            i++; j--;
+        }
+    }
+    if (low < j) quickSort(arr, low, j, mode);
+    if (i < high) quickSort(arr, i, high, mode);
+}
+
+// 4. PENCARIAN SEQUENTIAL
+// Mencari berdasarkan ID
+int cariBerdasarkanID(Obat arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i].id == key) return i;
+    }
+    return -1;
+}
+
+// Mencari berdasarkan Nama
+int cariBerdasarkanNama(Obat arr[], int n, char key[]) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(arr[i].nama, key) == 0) return i;
+    }
+    return -1;
+}
+
+// 5. FUNGSI HAPUS OBAT
+void hapusObat(int idHapus) {
+    if (listKosong()) {
+        cout << "Apotek kosong, tidak ada yang bisa dihapus.\n";
+        return;
+    }
+    typeptr bantu = awal;
+    while (bantu != NULL && bantu->info.id != idHapus) {
+        bantu = bantu->kanan;
+    }
+    if (bantu == NULL) {
+        cout << "Obat dengan ID " << idHapus << " tidak ditemukan.\n";
+        return;
+    }
+    if (bantu == awal) {
+        if (awal == akhir) { awal = akhir = NULL; }
+        else { awal = awal->kanan; awal->kiri = NULL; }
+    } else if (bantu == akhir) {
+        akhir = akhir->kiri; akhir->kanan = NULL;
+    } else {
+        bantu->kiri->kanan = bantu->kanan;
+        bantu->kanan->kiri = bantu->kiri;
+    }
+    delete bantu;
+    cout << "ID " << idHapus << " berhasil dihapus.\n";
+}
+
+// Data Sampel
+void tambahDataSampel() {
+    Obat sampel[5] = {
+        {101, "Paracetamol", 50, 5000},
+        {105, "Amoxicillin", 30, 12000},
+        {102, "Vitamin C", 100, 2500},
+        {104, "Obat Batuk", 25, 15000},
+        {103, "Antasida", 40, 8000}
+    };
+    for(int i = 0; i < 5; i++) tambahObat(sampel[i]);
+}
+
+// OPERASI FILE
+void simpanKeFile() {
+    FILE *pf = fopen("apotek_data.dat", "wb");
+    if (pf == NULL) return;
+    typeptr bantu = awal;
+    while (bantu != NULL) {
+        fwrite(&bantu->info, sizeof(Obat), 1, pf);
+        bantu = bantu->kanan;
+    }
+    fclose(pf);
+}
+
+void loadDariFile() {
+    FILE *pf = fopen("apotek_data.dat", "rb");
+    if (pf == NULL) {
+		tambahDataSampel();
+		return;
+	}
+    Obat temp;
+    buatListBaru();
+    while (fread(&temp, sizeof(Obat), 1, pf) == 1) {
+        tambahObat(temp);
+    }
+    fclose(pf);
+}
+
+// FUNGSI UTAMA & TAMPILAN
+int hitungNode() {
+    int count = 0;
+    typeptr bantu = awal;
+    while (bantu != NULL) { count++; bantu = bantu->kanan; }
+    return count;
+}
+
+void tampilkanData() {
+    if (listKosong()) { cout << "Apotek kosong.\n"; return; }
+    typeptr bantu = awal;
+    cout << "\nID\tNama Obat\t\tStok\tHarga\n";
+    cout << "----------------------------------------------------\n";
+    while (bantu != NULL) {
+        printf("%d\t%-20s\t%d\tRp%ld\n", 
+               bantu->info.id, bantu->info.nama, bantu->info.stok, bantu->info.harga);
+        bantu = bantu->kanan;
+    }
+}
+
+void tekanEnter() {
+    cout << "\nTekan [Enter] untuk kembali ke menu...";
+    cin.ignore();
+    cin.get();
+}
+
+int main() {
+    loadDariFile();
+    int pilihan;
+    do {
+		system("cls");
+        cout << "==========================================\n";
+        cout << "       SISTEM MANAJEMEN APOTEK            \n";
+        cout << "==========================================\n";
+        cout << "1. Tambah Obat Baru\n";
+        cout << "2. Tampilkan Semua Obat\n";
+        cout << "3. Menu Pengurutan\n";
+        cout << "4. Menu Pencarian\n";
+        cout << "5. Hapus Obat\n";
+        cout << "6. Simpan & Keluar\n";
+        cout << "------------------------------------------\n";
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+
+        if (pilihan == 1) {
+			system("cls");
+            Obat o;
+            cout << "ID Obat: "; cin >> o.id;
+            cout << "Nama Obat: "; cin.ignore(); cin.getline(o.nama, 30);
+            cout << "Stok: "; cin >> o.stok;
+            cout << "Harga: "; cin >> o.harga;
+            tambahObat(o);
+            cout << "\nData berhasil ditambahkan!";
+            tekanEnter();
+        } else if (pilihan == 2) {
+			system("cls");
+            tampilkanData();
+            tekanEnter();
+        } else if (pilihan == 3) { // SUB-MENU SORTING
+            if (listKosong()) { cout << "Data kosong!\n"; continue; }
+            int n = hitungNode();
+            Obat *tempArr = new Obat[n];
+            typeptr bantu = awal;
+            for (int i = 0; i < n; i++) { tempArr[i] = bantu->info; bantu = bantu->kanan; }
+            
+            int pSort;
+            system("cls");
+            cout << "\n--- SUB-MENU PENGURUTAN ---\n";
+            cout << "1. Berdasarkan ID (Ascending)\n";
+            cout << "2. Harga (Ascending)\n";
+            cout << "3. Harga (Descending)\n";
+            cout << "Pilih: ";
+            cin >> pSort;
+            
+            quickSort(tempArr, 0, n - 1, pSort);
+            buatListBaru();
+            for (int i = 0; i < n; i++) tambahObat(tempArr[i]);
+            delete[] tempArr;
+            cout << "Berhasil diurutkan.\n";
+            tampilkanData();
+            tekanEnter();
+        } else if (pilihan == 4) { // SUB-MENU SEARCHING
+            if (listKosong()) { cout << "Data kosong!\n"; continue; }
+            int n = hitungNode();
+            Obat *tempArr = new Obat[n];
+            typeptr bantu = awal;
+            for (int i = 0; i < n; i++) { tempArr[i] = bantu->info; bantu = bantu->kanan; }
+            
+            int pCari;
+            system("cls");
+            cout << "\n--- SUB-MENU PENCARIAN ---\n";
+            cout << "1. Cari Berdasarkan ID\n";
+            cout << "2. Cari Berdasarkan Nama\n";
+            cout << "Pilih: ";
+            cin >> pCari;
+            
+            if (pCari == 1) {
+                int idK; cout << "Masukkan ID: "; cin >> idK;
+                int h = cariBerdasarkanID(tempArr, n, idK);
+                if (h != -1) cout << "Ditemukan: " << tempArr[h].nama << " | Harga: Rp" << tempArr[h].harga << endl;
+                else cout << "Tidak ditemukan.\n";
+            } else if (pCari == 2) {
+                char namaK[30]; cout << "Masukkan Nama: "; cin.ignore(); cin.getline(namaK, 30);
+                int h = cariBerdasarkanNama(tempArr, n, namaK);
+                if (h != -1) cout << "Ditemukan ID: " << tempArr[h].id << " | Harga: Rp" << tempArr[h].harga << endl;
+                else cout << "Tidak ditemukan.\n";
+            }
+            delete[] tempArr;
+            tekanEnter();
+        }
+        else if (pilihan == 5) {
+            system("cls");
+            tampilkanData();
+            int idH; cout << "\nMasukkan ID yang akan dihapus: "; cin >> idH;
+            hapusObat(idH);
+            tekanEnter();
+        }
+    } while (pilihan != 6);
+
+    simpanKeFile();
+    system("cls");
+    cout << "==========================================\n";
+    cout << "  DATA TERSIMPAN. PROGRAM SELESAI.       \n";
+    cout << "==========================================\n";
+    return 0;
+}
